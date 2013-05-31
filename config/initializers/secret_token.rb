76b2b44a2f23ea17284aef0fb61bdf1e2abd0c4a
Nicholas Hippenmeyer <nicholashippenmeyer@gmail.com>
2013-05-30 19:06:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Identafy::Application.config.secret_key_base = 'f4af045979299874e816d1b8b264818d557d4c102b57435b94a5ea6b539658b335710ba662966488617585df06b4b8f6dd2e4d2176f221938bea1ad244c8a84c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Identafy::Application.config.session_store :cookie_store, key: '_Identafy_session'
